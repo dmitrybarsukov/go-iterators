51fc6132ee4f79c0384e8de4b2cddd7ed49790c2
dmibarsukov <dmibarsukov@ozon.ru>
2022-06-19 21:01:30 +0700

    Filtering iterator and it's tests

--- extended/filtering_iterator_test.go
+++ extended/filtering_iterator_test.go
@@ -1,3 +1,39 @@
 package extended
 
-// TODO
+import (
+	"testing"
+
+	"iterator/basic"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilteringIteratorNone(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	iter := FilteringIterator(basic.Slice(items), func(i int) bool { return false })
+	result := make([]int, 0)
+	for iter.HasNext() {
+		result = append(result, iter.Next())
+	}
+	assert.Empty(t, result)
+}
+
+func TestFilteringIteratorAll(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	iter := FilteringIterator(basic.Slice(items), func(i int) bool { return true })
+	result := make([]int, 0)
+	for iter.HasNext() {
+		result = append(result, iter.Next())
+	}
+	assert.Equal(t, items, result)
+}
+
+func TestFilteringIteratorSome(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	iter := FilteringIterator(basic.Slice(items), func(i int) bool { return i%2 == 0 })
+	result := make([]int, 0)
+	for iter.HasNext() {
+		result = append(result, iter.Next())
+	}
+	assert.Equal(t, []int{2, 4}, result)
+}
